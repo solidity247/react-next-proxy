8d4e8510bab3977abbcf701971ebbbef49606a30
solidity247 <104479422+solidity247@users.noreply.github.com>
2023-04-19 21:38:12 -0400

    test

--- components/GetDataNext.js
+++ components/GetDataNext.js
@@ -4,21 +4,23 @@ import FetchWrapper from "./UI/FetchWrapper";
 export default function GetDataNext() {
   const [data, setData] = useState([]);
   // "/api/todos"
+  const [dummyData, setDummyData] = useState({})
 
   const fetchData = async () => {
-    const url = '/api/todos';
+    const url = "/api/todos";
     const startTime = performance.now();
 
     try {
       const response = await fetch(url);
       const data = await response.json();
+      setDummyData(data);
       const endTime = performance.now();
       const duration = endTime - startTime;
       const size = new TextEncoder().encode(JSON.stringify(data)).length;
       const info = {
         url,
         duration,
-        size
+        size,
       };
       setData(info);
     } catch (error) {
@@ -33,11 +35,11 @@ export default function GetDataNext() {
     <FetchWrapper>
       <h3>Getting Data with NextJS Proxy</h3>
       <button onClick={clickDataHandler}>Click to get data!!!</button>
-      <ul>
-        {/* {data.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))} */}
-      </ul>
+      <h3>Request to {data.url} paremeters:</h3>
+      <p>Time: {Math.round(data.duration) || ""} ms</p>
+      <p>Data: {data.size} bytes</p>
+      {/* <p>Dummy data</p> */}
+      {/* <p>{JSON.stringify(dummyData)}</p> */}
     </FetchWrapper>
   );
 }
--- components/GetDataReact.js
+++ components/GetDataReact.js
@@ -1,19 +1,29 @@
-import FetchWrapper from "./UI/FetchWrapper";
 import { useState } from "react";
+import FetchWrapper from "./UI/FetchWrapper";
 
 export default function GetDataNext() {
   const [data, setData] = useState([]);
+  // "/api/todos"
 
   const fetchData = async () => {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/todos"
-      // "http://localhost:3000/api/todos"
-    );
-    const blob = await response.blob();
-    const dataSize = blob.size;
-    console.log(`Data size: ${dataSize} bytes`);
-    // const data = await blob.json();
-    // setData(data);
+    const url = "https://jsonplaceholder.typicode.com/todos";
+    const startTime = performance.now();
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const endTime = performance.now();
+      const duration = endTime - startTime;
+      const size = new TextEncoder().encode(JSON.stringify(data)).length;
+      const info = {
+        url,
+        duration,
+        size,
+      };
+      setData(info);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const clickDataHandler = () => {
@@ -21,13 +31,11 @@ export default function GetDataNext() {
   };
   return (
     <FetchWrapper>
-      <h3>Getting Data with React directly from Backend</h3>
+      <h3>Getting Data with NextJS Proxy</h3>
       <button onClick={clickDataHandler}>Click to get data!!!</button>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ul>
+      <h3>Request to {data.url} paremeters:</h3>
+      <p>Time: {Math.round(data.duration) || ""} ms</p>
+      <p>Data: {data.size} bytes</p>
     </FetchWrapper>
   );
 }
